e6e171c687347ba218d4b5c71b37009f2b05c30d
Ubuntu <ubuntu@ip-172-31-24-139.us-east-2.compute.internal>
2020-08-26 19:06:45 +0000

    factory functions and constructor functions

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y:1
-    },
-    draw: function() {
+//factory function
+function createCircle(radius) {
+    return { radius,
+    draw: function(){
         console.log("draw");
-    }
-};
+    },
+    };
+}
 
-circle.draw();
+const circle = createCircle(1);
+
+//constructor function
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log("draw");
+      }
+    }
+
+const another = new Circle(1);
+ another.draw()
